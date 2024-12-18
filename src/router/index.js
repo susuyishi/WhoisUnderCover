30a6b15b59f7ab518dcc719b40c85404659eacb2
ccbling <blingcc233@gmail.com>
2024-12-18 17:26:39 +0800

    feat(router): 添加退出房间功能- 在路由导航中添加 beforeEach钩子 - 实现从 GameRoom 页面返回 JoinRoom 页面时的退出房间逻辑 - 使用 js-cookie 和 WebSocketClient 进行用户和房间的退出处理

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import JoinRoom from "../views/JoinRoom.vue";
 import GameRoom from "../views/GameRoom.vue";
+import Cookies from "js-cookie";
+import WebSocketClient from "../utils/websocket.js";
+const wsClient = new WebSocketClient("ws://localhost:3000");
 
 const routes = [
     {
@@ -19,6 +22,28 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+    if (to.name === "JoinRoom" && from.name === "GameRoom") {
+        if (Cookies.get("userid") !== undefined && Cookies.get("userid") !== "undefined") {
+            const rid = Cookies.get("roomid");
+            const uid = Cookies.get("userid");
+            if (!wsClient.isConnected) {
+                await wsClient.connect();
+            }
+            wsClient.send({
+                type: "exit_room",
+                roomId: rid,
+                userId: uid
+            })
+            Cookies.set("userid", undefined);
+            Cookies.set("roomid", undefined);
+        }
+        next();
+    } else {
+        next();
+    }
 });
 
 export default router;
